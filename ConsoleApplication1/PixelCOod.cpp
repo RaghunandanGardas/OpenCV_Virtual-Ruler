742b24dc237da1481a89886aa06928b662cdc765
Raghunandan1606 <raghu.mufc@gmail.com>
2017-04-21 03:59:21 -0400

    Virtual Ruler Project

--- ConsoleApplication1/PixelCOod.cpp
+++ ConsoleApplication1/PixelCOod.cpp
@@ -1,53 +1,81 @@
-
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
 #include "stdafx.h"
 
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/core/core.hpp"
-#include <iostream>
-
 using namespace cv;
-using namespace std;
 
+/// Global variables
 
-Mat img;
+int threshold_value = 0;
+int threshold_type = 3;;
+int const max_value = 255;
+int const max_type = 4;
+int const max_BINARY_value = 255;
 
-void onMouse(int event, int x, int y, int flags, void* param)
-{
-	char text[100];
-	Mat img2, img3;
+Mat src, src_gray, dst;
+char* window_name = "Threshold Demo";
 
-	img2 = img.clone();
+char* trackbar_type = "Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted";
+char* trackbar_value = "Value";
 
-	if (event == CV_EVENT_LBUTTONDOWN)
-	{
-		Vec3b p = img2.at<Vec3b>(y, x);
-		sprintf(text, "R=%d, G=%d, B=%d", p[2], p[1], p[0]);
-	}
-	else if (event == CV_EVENT_RBUTTONDOWN)
-	{
-		cvtColor(img, img3, CV_BGR2HSV);
-		Vec3b p = img3.at<Vec3b>(y, x);
-		sprintf(text, "H=%d, S=%d, V=%d", p[0], p[1], p[2]);
-	}
-	else
-		sprintf(text, "x=%d, y=%d", x, y);
+/// Function headers
+void Threshold_Demo(int, void*);
 
-	putText(img2, text, Point(5, 15), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0));
-	imshow("image", img2);
-}
-
+/**
+* @function main
+*/
 int main(int argc, char** argv)
 {
-	img = imread("C:/Users/Raghunandan/Desktop/Bounding.jpg");
-	if (img.empty())
-		return -1;
+	/// Load an image
+	src = imread("C:/Users/Raghunandan/Desktop/Right_Bottle.jpg");
 
-	namedWindow("image");
-	setMouseCallback("image", onMouse, 0);
-	imshow("image", img);
-	waitKey(0);
+	/// Convert the image to Gray
+	cvtColor(src, src_gray, CV_BGR2GRAY);
 
-	return 0;
+	/// Create a window to display results
+	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+
+	/// Create Trackbar to choose type of Threshold
+	createTrackbar(trackbar_type,
+		window_name, &threshold_type,
+		max_type, Threshold_Demo);
+
+	createTrackbar(trackbar_value,
+		window_name, &threshold_value,
+		max_value, Threshold_Demo);
+
+	/// Call the function to initialize
+	Threshold_Demo(0, 0);
+
+	/// Wait until user finishes program
+	while (true)
+	{
+		int c;
+		c = waitKey(20);
+		if ((char)c == 27)
+		{
+			break;
+		}
+	}
+
+}
+
+
+/**
+* @function Threshold_Demo
+*/
+void Threshold_Demo(int, void*)
+{
+	/* 0: Binary
+	1: Binary Inverted
+	2: Threshold Truncated
+	3: Threshold to Zero
+	4: Threshold to Zero Inverted
+	*/
+
+	threshold(src_gray, dst, threshold_value, max_BINARY_value, threshold_type);
+
+	imshow(window_name, dst);
 }
